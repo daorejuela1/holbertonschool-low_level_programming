e8ef00f18a91b3cc488c2851e51907dc5aceba0b
Maquesino <vids@hotmail.es>
2020-02-05 18:04:27 +0000

    Added first 5 task for low level track

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	char my_message[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_message);
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,2 @@
+Here you can find information about:
+Preprocessor - Compiler  - Assembler - Name -  Hello, puts - Hello, printf - various types on the computer
