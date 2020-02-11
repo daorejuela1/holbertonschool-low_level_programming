40382e39a44870818597684cb2e5c13caf483a68
Maquesino <vids@hotmail.es>
2020-02-11 13:47:15 +0000

    FIXED task 1

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,7 @@
 #include "holberton.h"
 /**
  * main - writes the characters Holberton to stdout
- * 
- *
  * Return: always 0.
- * 
  */
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_last_digit - prints_last_digit
+ * @n: number to evalue
+ * Return: last digit.
+ *
+ */
+void jack_bauer(void)
+{
+	int decenas_horas,unidades_horas;
+	int decenas_minutos, unidades_minutos;
+
+	for (horas = 0;
+}
