38c5c221780176158f875fa1ad4dd6888de145fd
Maquesino <vids@hotmail.es>
2020-02-12 21:20:48 +0000

    Added task Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long long int backup, i, acumulator = 0;
+	unsigned long long int memory = 1, fibo = 2;
+
+	acumulator = acumulator + 2;
+	for (i = 0; i < 48; i++)
+	{
+		backup = fibo;
+		fibo = fibo + memory;
+		memory = backup;
+		if (fibo <= 4000000)
+			if (fibo % 2 == 0)
+				acumulator = acumulator + fibo;
+	}
+	printf("%llu\n", acumulator);
+	return (0);
+}
