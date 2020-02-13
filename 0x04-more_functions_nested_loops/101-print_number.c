a22789afbc3a2dfcacbacdf4cfdd287fda78f547
Maquesino <vids@hotmail.es>
2020-02-13 20:20:46 +0000

    Uploaded last task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_number - prinst an int number by using putchar
+ *@n: input number
+ *Return: None
+ */
+void print_number(int n)
+{
+	int number = n;
+
+	if (number < 0)
+	{
+		number = number * -1;
+		putchar('-');
+	}
+	if (number > 999)
+	{
+		_putchar(number / 1000 % 10 + '0');
+		_putchar(number / 100 % 10 + '0');
+		_putchar(number / 10 % 10 + '0');
+		_putchar(number % 10 + '0');
+	}
+	else if (number > 99)
+	{
+		_putchar(number / 100 % 10 + '0');
+		_putchar(number / 10 % 10 + '0');
+		_putchar(number % 10 + '0');
+	}
+	else if (number > 9)
+	{
+		_putchar(number / 10 % 10 + '0');
+		_putchar(number % 10 + '0');
+	}
+	else
+	{
+		_putchar(number % 10 + '0');
+	}
+}
