ad2563f7fc5f7f0353f7c880a9a0850714ea6f9f
Maquesino <vids@hotmail.es>
2020-02-19 21:43:30 +0000

    FIXED spaces advance 1

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,26 +6,19 @@
  */
 int _atoi(char *s)
 {
-        int tamano = 0, i, number_found = 0, number = 0, sign = 1;
+	int tamano = 0, i, number_found = 0, number = 0, sign = 1;
 
-        for (tamano = 0; s[tamano] < 48 || s[tamano] > 57; tamano++)
-        {
-                if (s[tamano] == '-')
-                {
-                        sign = sign * -1;
-                }
-        }
-        for (i = tamano; s[i] != 0; i++)
-        {
-                if (s[i] >= 48 && s[i] <= 48 + 9)
-                        {
-                                number = number * 10 + s[i] - '0';
-                        }
-                else if (number != 0)
-                        {
-                                break;
-                        }
-
-        }
-        return (number * sign);
+	for (tamano = 0; s[tamano] < 48 || s[tamano] > 57; tamano++)
+	{
+		if (s[tamano] == '-')
+			sign = sign * -1;
+	}
+	for (i = tamano; s[i] != 0; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 48 + 9)
+			number = number * 10 + s[i] - '0';
+		else if (number != 0)
+			break;
+	}
+	return (number * sign);
 }
