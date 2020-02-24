050f082ca9d2a07ae6981c6118b8db7b704cd649
Maquesino <vids@hotmail.es>
2020-02-24 21:59:08 +0000

    FIXED task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	char **pointer_to_s;
 	unsigned int i;
 
-	pointer_to_s = &s;
 	for (i = 0; s[i] != 0; i++)
 	{
-		if (*(*pointer_to_s + i) == c)
-			return (*pointer_to_s + i);
+		if (*(s + i) == c)
+			return (s + i);
 	}
 			return (NULL);
 }
