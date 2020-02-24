fc251ebd5e4284fa13b641b88410b787c34e1334
Maquesino <vids@hotmail.es>
2020-02-24 22:02:10 +0000

    FIXED task 2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,13 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	char **pointer_to_s, **pointer_to_accept;
-	unsigned int letter, search, n = 0, m = 0, result = 0;
+	unsigned int letter, search, result = 0;
 
-	while (s[n] != 0)
-		n++;
-	while (accept[m] != 0)
-		m++;
 	pointer_to_s = &s;
 	pointer_to_accept = &accept;
-	for (letter = 0; letter < m; letter++)
+	for (letter = 0; s[letter] != 0; letter++)
 	{
-		for (search = 0; search < n; search++)
+		for (search = 0; accept[search] != 0; search++)
 		{
 			if (*(*pointer_to_s + letter)
 				== (*(*pointer_to_accept + search)))
