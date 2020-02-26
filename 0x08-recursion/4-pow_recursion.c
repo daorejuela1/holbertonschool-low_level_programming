4946f6dca5d9289cdaba104b74687678368c9c84
Maquesino <vids@hotmail.es>
2020-02-26 23:13:47 +0000

    ADDED task 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_pow_recursion - find the pow of x,y  without using loops
+ *@x: base number
+ *@y: power
+ *Return:  of input string
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+	else 
+		return (x * _pow_recursion(x , y - 1));
+}
