10c74847ccf933d721c0b91e62b77d2dbf70926e
Maquesino <vids@hotmail.es>
2020-02-26 23:37:25 +0000

    ADD second task and corrected 4 task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+int my_function(char *s, int a);
 /**
  *_strlen_recursion - find the lenght of the string without using loops
  *@s: string to print
@@ -8,14 +9,30 @@
  */
 int _strlen_recursion(char *s)
 {
-	int result = 0;
-	if (*(s + 1) != 0)
+	int a = 0, result;
+
+	if (*s)
 	{
-		 return (result);
+		result = my_function(s, a);
 	}
 	else
 	{
-		result++;
-		return (result);
+		return (0);
 	}
+	return (result);
+}
+
+
+/**
+ *my_function - find the lenght of the string without using loops
+ *@s: strin to print
+ *@a: letter count
+ *Return: lenght of input string
+ *
+ */
+int my_function(char *s, int a)
+{
+	if (*(s) != 0)
+		return (my_function(s + 1, a + 1));
+	return (a);
 }
