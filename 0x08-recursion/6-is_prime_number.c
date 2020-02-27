e4948367ea83acd64e7ff06d74dc2173bd3834e2
Maquesino <vids@hotmail.es>
2020-02-27 00:41:00 +0000

    ADDED question 7

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+int my_function(int n, int a, int counter);
+/**
+ *is_prime_number - find if the number is prime
+ *@n: base number
+ *Return: 1 if number is 0 in other case
+ *
+ */
+int is_prime_number(int n)
+{
+	int a = 1, counter = 0;
+	int result;
+
+	if (n < 0)
+		n = n * -1;
+	result = my_function(n, a, counter);
+	if (result == 2)
+		result = 1;
+	else
+		result = 0;
+	return (result);
+}
+
+/**
+ *my_function - find the sqrt of n without using loops
+ *@n: base number
+ *@a: iterator
+ *@counter: division number
+ *Return:  of input string
+ */
+int my_function(int n, int a, int counter)
+{
+	if (n % a == 0)
+		counter++;
+	else if (n < a)
+		return (counter);
+	return (my_function(n, a + 1, counter));
+}
