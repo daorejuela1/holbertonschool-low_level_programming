8a1463b23dfce597324cf5bdcbeb4a28fbefa980
Maquesino <vids@hotmail.es>
2020-03-03 21:41:45 +0000

    fixed logic for task 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,18 +10,19 @@ char *_strdup(char *str)
 {
 	unsigned int i = 0, n = 0;
 	char *array;
+
 	if (str == NULL)
 		return (NULL);
 	if (*str == 0)
 		return (NULL);
 	while (str[n])
 		n++;
-	array = (char *)malloc((n + 1) * sizeof(char));
-	for (i = 0; str[i]; i++)
-		array[i] = 0;
+	array = (char *)malloc((n + 2) * sizeof(char));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i <= n + 1; i++)
+	for (i = 0; i < n + 2; i++)
+		array[i] = 0;
+	for (i = 0; i < n + 2; i++)
 		array[i] = str[i];
 	return (array);
 }
