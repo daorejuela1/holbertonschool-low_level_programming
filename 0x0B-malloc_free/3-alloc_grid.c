3fadde8c1833dffe7cfb893c60e755639686a0d1
Maquesino <vids@hotmail.es>
2020-03-04 00:04:17 +0000

    ADDED third task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *alloc_grid - allocate matrix in the form of memory
+ *@width: matrix col
+ *@height: matrix row
+ *Return: matrix
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matrix, i = 0, row = 0, col = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = (int **)malloc(width * sizeof(int));
+	for (i = 0; i < height; i++)
+		matrix[i] = (int *)malloc(height * sizeof(int));
+	for (row = 0; row < width; row++)
+		for (col = 0; col < height; height++)
+			matrix[row][col] = 0;
+	return (matrix);
+}
