d62b5c8d946e38d8f88c353a4b057c7d30b94c62
daorejuela1 <vids@hotmail.es>
2020-03-04 00:02:39 -0500

    Init last task

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *argstostr - allocate matrix in the form of memory
+ *@ac: matrix col
+ *@av: matrix row
+ *Return: matrix
+ */
+char *argstostr(int ac, char **av)
+{
+	char *array;
+	int i = 0, row = 0, count = 0, acum = 0;
+
+	if (ac == 0 || av  == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][count] != 0)
+			count++;
+		acum = count + acum;
+		count = 0;
+	}
+	acum = acum + i;
+	array = (char *)malloc(count * sizeof(char));
+	printf("Total de caracters %d\n", acum);
+	if (array == NULL)
+		return (NULL);
+	for (row = 0; row < ac; row++)
+	{
+		while (av[row][count])
+		{
+			array[count] = av[row][count];
+			count++;
+		}
+		array[count + 1] = '\n';
+	}
+	return (array);
+}
