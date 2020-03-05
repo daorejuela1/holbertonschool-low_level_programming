f7d15621fd003688c0e46d1843d44dd8dcea08ef
Maquesino <vids@hotmail.es>
2020-03-05 21:06:19 +0000

    Fixed third task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+char *_memset(char *s, char b, unsigned int n);
 /**
  *_calloc - using calloc with malloc
  *@nmemb: number of entries
@@ -7,15 +8,33 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *pointer;
-	unsigned int i;
+	void *pointer;
 
 	if (size <= 0 || nmemb <= 0)
 		return (NULL);
-	pointer = malloc(size * nmemb);
+	pointer = malloc(nmemb * size);
 	if (pointer == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		pointer[i] = 0;
+	pointer = _memset(pointer, 0, nmemb);
 	return (pointer);
+}
+
+/**
+ *_memset -  sets memory values from *s to n quantity
+ *@s: initial address
+ *@b: value
+ *@n: quantity of bytes
+ *Return: pointer to the memory area of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char **pointer_to_s;
+	unsigned int i;
+
+	pointer_to_s = &s;
+	for (i = 0; i < n; i++)
+	{
+	*(*pointer_to_s + i) = b;
+	}
+	return (*pointer_to_s);
 }
