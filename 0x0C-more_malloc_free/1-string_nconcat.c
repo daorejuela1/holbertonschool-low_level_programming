88690da652ce56fb2d22f3697f5ebb23cc022163
Maquesino <vids@hotmail.es>
2020-03-05 18:31:09 +0000

    Fixed task 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,11 +4,12 @@ unsigned int get_len(char *s);
  *string_nconcat - cat strings till n len in s2
  *@s1: string 1
  *@s2: string 2
+ *@n: len of s2
  *Return: concat string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len, s2_len, i, p;
+	unsigned int s1_len, s2_len, i = 0, p = 0;
 	char *result;
 
 	s1_len = get_len(s1);
@@ -18,10 +19,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	result = malloc(s1_len + n + 1);
 	if (result == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
-		result[i] = s1[i];
-	for (p = 0; p < n; p++)
-		result[i + p] = s2[p];
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i]; i++)
+			result[i] = s1[i];
+	}
+	if (s2 != NULL)
+	{
+		for (p = 0; p < n; p++)
+			result[i + p] = s2[p];
+	}
 	result[i + p] = 0;
 	return (result);
 }
@@ -34,7 +41,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int get_len(char *s)
 {
 	unsigned int i = 0;
+
+	if (s == NULL)
+		return (0);
 	while (s[i])
 		i++;
-	return i;
+	return (i);
 }
