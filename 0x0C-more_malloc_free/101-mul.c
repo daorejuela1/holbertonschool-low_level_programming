5577e8efc45acaa11e2035dd8f6d324630b3f5ad
Maquesino <vids@hotmail.es>
2020-03-06 01:33:16 +0000

    ADDED last advance logic

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *main - main function
+ *@argc: quantity
+ *@argv: values
+ *Return: 0
+ */
+int main (int argc, char **argv)
+{
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
