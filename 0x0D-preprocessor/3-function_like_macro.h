6e070c484a043ff7f859c0c2e11970d2a18241d7
Maquesino <vids@hotmail.es>
2020-03-09 17:31:51 +0000

    FIXED syntax for task 2,3 and 4

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_
 #define _FUNCTION_LIKE_MACRO_
 
-#define ABS(a) ((a) < 0 ? a * - 1 : a)
+#define ABS(a)((a) < 0 ? (a) * -1 : (a))
 
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,6 @@
 #ifndef _SUM_
-#define _SUM
+#define _SUM_
 
-#define SUM(a, b) ((a) + (b))
+#define SUM((a), (b)) ((a) + (b))
 
 #endif
