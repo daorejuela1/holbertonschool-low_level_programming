89ee4ee5a1b02fb697bf3241681e533ba77bec50
Maquesino <vids@hotmail.es>
2020-03-11 21:43:07 +0000

    ADDED case to check if null values is received

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,8 @@ int main(int argc, char *argv[])
 	int a, b, result;
 	int (*pointer_to_function)(int, int);
 
-	if (argc - 1 != 3)
+	if (argc - 1 != 3  || argv[1] == NULL
+			|| argv[3] == NULL || argv[2] == NULL)
 	{
 		printf("Error\n");
 		exit(98);
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	pointer_to_function = get_op_func(argv[2]);
-	if (pointer_to_function == NULL || argv[1] == NULL || argv[3] == NULL)
+	if (pointer_to_function == NULL)
 		return (0);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
