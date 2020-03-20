1a9d34c15b3bf17e14a8deaa5e6ebee5bef889a1
daorejuela1 <vids@hotmail.es>
2020-03-19 19:40:35 -0500

    ADDED task 4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,32 +1,65 @@
 #include "lists.h"
+
+/**
+ *put_node - adds node at end of list and return
+ *pointer to origin
+ *@head: pointer to the pointer of the list
+ *@new_node: str data to add
+ *@old_clone: old data copy with original address
+ *Return: none
+ */
+void put_node(list_t **head, list_t *new_node, list_t *old_clone)
+{
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (*head != NULL)
+		{
+			if ((*head)->next == NULL)
+			{
+				(*head)->next = new_node;
+				(*head) = old_clone;
+				break;
+			}
+			*head = (*head)->next;
+		}
+	}
+}
 /**
  *add_node_end - adds new node at end of list
  *@head: pointer to the pointer of the list
- *@str - str data to add
+ *@str: str data to add
  *Return: pointer to the new element of list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int i, len_str = 0;
+	int len_str = 0;
+	list_t *new_node, *old_clone;
+	char *final = NULL;
 
 	if (head == NULL)
 		return (NULL);
-	char *final = strdup(*head);
-	list_t new_node = malloc(sizeof(list_t));
-
+	if (str != NULL)
+	{
+		final = strdup(str);
+		if (final == NULL)
+			return (NULL);
+		while (final[len_str])
+			len_str++;
+	}
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	while (final[len_str])
-		len_str++;
+	old_clone = malloc(sizeof(list_t));
+	if (old_clone == NULL)
+		return (NULL);
+	old_clone = *head;
 	new_node->str = final;
 	new_node->len = len_str;
 	new_node->next = NULL;
-	for (i = 0; head[i]; i++)
-	{
-		if (head->next == NULL)
-		{
-			head->next = new_node;
-		}
-	}
-	free (final);
+	put_node(head, new_node, old_clone);
+	return (new_node);
 }
