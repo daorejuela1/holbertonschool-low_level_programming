1cc0432dfecfac5a0574ca6c5758de422ec467de
daorejuela1 <vids@hotmail.es>
2020-03-25 03:39:17 +0000

    Changed conditions for third advance

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,28 @@
 #include "lists.h"
 /**
- *print_listint - prints a list that contains ints
- *@h: pointer to the first elements of the list
+ *print_listint_safe - prints a list that contains ints
+ *@head: pointer to the first elements of the list
  *Return: Quantity of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	int i = 0;
-    const listint_t *temp;
+	const listint_t *temp;
 
 	if (head == NULL)
 		return (0);
 	for (i = 0; head != NULL; i++)
 	{
 		temp = head;
-        printf("[%p] %d\n",(void *)head, head->n);
-        head = head->next;
-        if (temp <= head)
-            break;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		if (temp <= head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			i++;
+			break;
+		}
+
 	}
 	return (i);
 }
