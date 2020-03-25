f6b65f8364a2fe4cd40564be526babe8807916d0
daorejuela1 <vids@hotmail.es>
2020-03-25 03:32:00 +0000

    Used address comparition instead of array

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,27 +1,23 @@
 #include "lists.h"
 /**
- *print_listint_safe - prints a list that contains ints
- *@head: pointer to the first elements of the list
+ *print_listint - prints a list that contains ints
+ *@h: pointer to the first elements of the list
  *Return: Quantity of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int i = 0, j = 0, k = 0, loop_found = 0;
-	const listint_t *array[1000];
+	int i = 0;
+    const listint_t *temp;
 
+	if (head == NULL)
+		return (0);
 	for (i = 0; head != NULL; i++)
 	{
-		if (loop_found == 1)
-			break;
-		array[k] = head;
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		k++;
-		for (j = 0; j < k; j++)
-		{
-			if (array[j] == head)
-				loop_found = 1;
-		}
+		temp = head;
+        printf("[%p] %d\n",(void *)head, head->n);
+        head = head->next;
+        if (temp <= head)
+            break;
 	}
 	return (i);
 }
