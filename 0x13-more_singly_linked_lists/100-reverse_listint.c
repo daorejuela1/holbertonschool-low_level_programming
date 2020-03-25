ff7c4354f31a140bda7cf2e760b5890e63c231f4
daorejuela1 <vids@hotmail.es>
2020-03-25 02:09:49 +0000

    Added first advance

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *reverse_listint - reverse connections of the list
+ *@head: pointer to the first elements of the list
+ *Return: 1 if SUCCESS, or -1 if it failed
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous, *next_node;
+
+	previous = NULL;
+	next_node = (*head)->next;
+	(*head)->next = previous;
+	previous = *head;
+	while (*head != NULL)
+	{
+		*head = next_node;
+		next_node = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next_node;
+	}
+	*head = previous;
+	return (*head);
+}
