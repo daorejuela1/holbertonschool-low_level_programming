6c57b07899d2aa1e93ab051337a428495dba739d
daorejuela1 <vids@hotmail.es>
2020-03-26 23:50:46 +0000

    Added fifth task

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#define UREALONG unsigned long int
+#define REALONG unsigned int
+/**
+ *flip_bits - clear bit in desired index
+ *@n: input number A
+ *@m: input number B
+ *Return: Quantity of bits
+ */
+REALONG flip_bits(UREALONG n, UREALONG m)
+{
+	UREALONG difference, i;
+	int count = 0;
+
+	difference = n ^ m;
+	for (i = 0; i < (sizeof(UREALONG) * 8) - 1; i++)
+	{
+		if ((difference >> i) & 1)
+			count++;
+
+	}
+	return (count);
+}
