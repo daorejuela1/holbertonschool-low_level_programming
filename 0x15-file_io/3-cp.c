4f2dd7fffb892ccb290c19fc511295805bb58b95
daorejuela1 <vids@hotmail.es>
2020-03-31 22:47:30 +0000

    Added fourth task

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "holberton.h"
+
+/**
+ *close_secure - securely close both files
+ *@file_to: pid first file
+ *@file_from: pid second file
+ *Return: 0 if success
+ */
+void close_secure(int file_to, int file_from)
+{
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+}
+
+
+/**
+ *main - cp
+ *@argc: Quantity of arguments
+ *@argv: Value of the arguments
+ *Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, rd_error = 1, result;
+	char buf[1024];
+
+	if (argc - 1 != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0666);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	file_from = open(argv[1], O_RDWR);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	while (rd_error != 0)
+	{
+		rd_error = read(file_from, buf, 1024);
+		if (rd_error == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		result = write(file_to, buf, rd_error);
+		if (result == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close_secure(file_to, file_from);
+	return (0);
+}
