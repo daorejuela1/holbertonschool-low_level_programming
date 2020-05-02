f613bc3fbf9dbc8b329855442259fc3a4b88b72b
daorejuela1 <vids@hotmail.es>
2020-05-02 22:31:36 +0000

    added condition for last index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			break;
 		(*h) = (*h)->next;
 	}
-	if (idx == i)
+	if (idx == i && *h == NULL)
 	{
 		(*h) = init_value;
 		return (add_dnodeint_end(h, n));
