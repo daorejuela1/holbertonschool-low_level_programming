43e065c08475a12c37cb4fd713794b6772b312dd
daorejuela1 <vids@hotmail.es>
2020-05-05 01:15:01 +0000

    added exec permission to unrandom so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/daorejuela1/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/unrandom.so
-export LD_PRELOAD=unrandom.so
+wget https://github.com/daorejuela1/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/unrandom.so -P ~
+export LD_PRELOAD=~/unrandom.so
