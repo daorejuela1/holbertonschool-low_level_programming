4bbe9e870870b39fc254183ac3ac015a340652c0
daorejuela1 <vids@hotmail.es>
2020-07-04 00:36:56 +0000

    added cases when 1 is in the border

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,22 +17,42 @@ def island_perimeter(grid):
         return perimeter
     for row in grid:
         num_col += 1
-        for actual in range(1, len(row) - 1):
+        for actual in range(len(row)):
             prev_val = actual - 1
             next_val = actual + 1
             if (row[actual] == 1):
-                if (row[prev_val] == 0):
-                    perimeter += 1
-                if (row[next_val] == 0):
+                try:
+                    if (row[prev_val] == 0 and prev_val >= 0):
+                        perimeter += 1
+                    elif (prev_val < 0):
+                        perimeter +=1
+                except IndexError:
+                        perimeter += 1
+                try:
+                    if (row[next_val] == 0 and prev_val >= 0):
+                        perimeter += 1
+                    elif (prev_val < 0):
+                        perimeter +=1
+                except IndexError:
                     perimeter += 1
     for i in range(num_col):
         column = [row[i] for row in grid]
-        for actual in range(1, len(column) - 1):
+        for actual in range(len(column)):
             prev_val = actual - 1
             next_val = actual + 1
             if (column[actual] == 1):
-                if (column[prev_val] == 0):
-                    perimeter += 1
-                if (column[next_val] == 0):
+                try:
+                    if (column[prev_val] == 0 and prev_val >= 0):
+                        perimeter += 1
+                    elif (prev_val < 0):
+                        perimeter +=1
+                except IndexError:
+                        perimeter += 1
+                try:
+                    if (column[next_val] == 0 and prev_val >= 0):
+                        perimeter += 1
+                    elif (prev_val < 0):
+                        perimeter +=1
+                except IndexError:
                     perimeter += 1
     return(perimeter)
